618d40b40336749b49789d4e614a3c1ecde57f7e
Lenny <lbanobre@live.com>
2020-05-19 17:08:03 -0400

    Update MapperHelper.cs

--- src/creaxu-framework-v1/Creaxu.Framework/Helpers/MapperHelper.cs
+++ src/creaxu-framework-v1/Creaxu.Framework/Helpers/MapperHelper.cs
@@ -7,6 +7,9 @@ namespace Creaxu.Framework.Helpers
     {
         public static T Map<T>(object source)
         {
+            if (source == null)
+                return default;
+
             var json = JsonConvert.SerializeObject(source);
             return JsonConvert.DeserializeObject<T>(json);
         }
