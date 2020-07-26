d0523a86897af8289e2350738f4c71669d54fd25
Lenny <lbanobre@live.com>
2020-07-26 14:19:23 -0400

    update package

---
+++ src/creaxu-framework-v1/Creaxu.Framework/Services/JwtTokenService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Creaxu.Framework.Services
+{
+    public interface IJwtTokenService
+    {
+        string BuildToken(IEnumerable<Claim> claims);
+    }
+
+    public class JwtTokenService : IJwtTokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Builds the token used for authentication
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public string BuildToken(IEnumerable<Claim> claims)
+        {
+            // Creates a key from our private key that will be used in the security algorithm next
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            // Credentials that are encrypted which can only be created by our server using the private key
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // this is the actual token that will be issued to the user
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(double.Parse(_configuration["Jwt:ExpireTime"])),
+                signingCredentials: creds);
+
+            // return the token in the correct format using JwtSecurityTokenHandler
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
